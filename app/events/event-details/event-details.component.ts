fd324aff0433cbde47e19c48666c17370b0f85ab
Jason Lee <jlee@fcdjfs.co.franklin.oh.us>
2017-03-20 13:36:47 -0400

    Added create session functionality

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService, IEvent } from '../shared/index';
-import { ActivatedRoute, } from '@angular/router';
+import { EventService, IEvent, ISession } from '../shared/index';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -8,19 +8,33 @@ import { ActivatedRoute, } from '@angular/router';
     styles: [`
         .container { padding-left: 20px; padding-right: 20px;}
         .event-image {height: 100px;}
+        a {cursor:pointer;}
     `]
 })
 export class EventDetailsComponent implements OnInit {
-    event:IEvent;
+    event: IEvent;
+    addMode: boolean;
     constructor(
         private eventService: EventService,
         private route: ActivatedRoute) { }
 
-    ngOnInit() { 
+    ngOnInit() {
         this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
     }
 
-    addSession(){
+    addSession() {
+        this.addMode = true;
+    }
 
+    saveNewSession(session: ISession) {
+        const nextId = Math.max.apply(null, this.event.sessions.map(x => x.id));
+        session.id = nextId + 1;
+        this.event.sessions.push(session);
+        this.eventService.updateEvent(this.event);
+        this.addMode = false;
+        //this.eventService.saveSession(this.event.id,session);
+    }
+    cancelAddSession() {
+        this.addMode = false;
     }
 }
